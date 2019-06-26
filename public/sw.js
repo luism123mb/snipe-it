965c9187e5c06c9779718ddd3812a02ec2df7825
luism123mb <36648228+luism123mb@users.noreply.github.com>
2019-06-26 09:32:18 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -22,4 +22,14 @@ self.addEventListener('fetch', function(event) {
 
 console.log(event.request.url);
 
+event.respondWith(
+
+caches.match(event.request).then(function(response) {
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
