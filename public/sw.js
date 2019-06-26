a9e2fce8c0d474341e7e8673fc7841603425deec
luism123mb <36648228+luism123mb@users.noreply.github.com>
2019-06-26 11:41:58 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,6 +5,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
+        `/`,
         `/`
       ])
           .then(() => self.skipWaiting());
