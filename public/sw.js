ac8bad51140c7635b041d7d79f2521a35f3c7cdc
luism123mb <36648228+luism123mb@users.noreply.github.com>
2019-06-26 11:40:57 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,14 +5,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-        `/`,
-        `/index.html`,
-        `/styles/main.css`,
-        `/scripts/main.min.js`,
-        `/scripts/comlink.global.js`,
-        `/scripts/messagechanneladapter.global.js`,
-        `/scripts/pwacompat.min.js`,
-        `/sounds/airhorn.mp3`
+        `/`
       ])
           .then(() => self.skipWaiting());
     })
