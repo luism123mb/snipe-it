d48cdefb4b814fd913970eb76f075103359b0633
luism123mb <36648228+luism123mb@users.noreply.github.com>
2019-06-26 11:33:35 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,9 +4,7 @@ self.addEventListener('install', function(e) {
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
        '/',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1'
+       '/'
      ]);
    })
  );
