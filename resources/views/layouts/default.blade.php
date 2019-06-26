85d3d5f66089231fb67003058521fab5c944510d
luism123mb <36648228+luism123mb@users.noreply.github.com>
2019-06-26 11:11:35 -0400

    www

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -875,6 +875,18 @@ border-color: #fff;"><i class="fa fa-search"></i></button>
       </div>
     </div>
   </div>
-
+  <script>
+  // ServiceWorker is a progressive technology. Ignore unsupported browsers
+  if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register('/sw.js').then(function() {
+      console.log('CLIENT: service worker registration complete.');
+    }, function() {
+      console.log('CLIENT: service worker registration failure.');
+    });
+  } else {
+    console.log('CLIENT: service worker is not supported. v2');
+  }
+  </script>
   </body>
 </html>
