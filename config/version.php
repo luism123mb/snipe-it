c3795e702d232cf8cb3175ff9ea295c55690bda9
luism123mb <36648228+luism123mb@users.noreply.github.com>
2019-06-26 23:44:40 -0400

    2019-06-26@11:44

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v4.6.15',
+  'full_app_version' => 'v4.6.15 - build 4011-gd1e9fbfa2',
+  'build_version' => '4011',
+  'prerelease_version' => '',
+  'hash_version' => 'gd1e9fbfa2',
+  'full_hash' => 'v4.6.15-2-gd1e9fbfa2',
+  'branch' => 'master',
+);
